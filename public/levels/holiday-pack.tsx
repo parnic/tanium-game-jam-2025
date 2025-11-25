cfec28813d45edaf37c35f05e808a70ab5862a9c
Parnic <github@parnic.com>
2025-11-24 21:10:40 -0600

    add holiday pack, setup gift tiles and spawn locations

---
+++ public/levels/holiday-pack.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="holiday-pack" tilewidth="128" tileheight="128" tilecount="60" columns="10">
+ <image source="../images/topdownPack_tilesheet@2.png" width="1280" height="768"/>
+ <tile id="40" type="gift">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision" x="38" y="32.6667" width="48.6667" height="64"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="40" duration="290"/>
+   <frame tileid="41" duration="290"/>
+   <frame tileid="42" duration="290"/>
+  </animation>
+ </tile>
+ <tile id="44" type="gift">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision" x="39" y="32.6667" width="48.6667" height="64"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="44" duration="290"/>
+   <frame tileid="45" duration="290"/>
+   <frame tileid="46" duration="290"/>
+  </animation>
+ </tile>
+ <tile id="50" type="gift">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="collision" x="38.3333" y="32.6667" width="48.6667" height="64"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="50" duration="290"/>
+   <frame tileid="51" duration="290"/>
+   <frame tileid="52" duration="290"/>
+  </animation>
+ </tile>
+</tileset>
