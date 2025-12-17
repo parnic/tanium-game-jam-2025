462f4eebc3375945dbc878f5ce77d04e51e29cb5
Parnic <github@parnic.com>
2025-12-17 13:34:07 -0600

    show glowing outline on exit when we have collected all gifts

--- src/exit.ts
+++ src/exit.ts
@@ -1,17 +1,22 @@
 import {
   CollisionType,
   type Engine,
+  type Material,
+  type Shader,
   Sprite,
   type Vector,
   vec,
 } from "excalibur";
 import { config } from "./config";
 import { GameActor } from "./game-actor";
+import { createOutlineMaterial } from "./materials/outline";
 import { OffScreenIndicator } from "./off-screen-indicator";
 import { Resources } from "./resources";
+import { GameLevel } from "./scenes/game-level";
 
 export class LevelExit extends GameActor {
   offScreen: OffScreenIndicator;
+  outlineMat?: Material;
 
   constructor(pos: Vector) {
     const sprite = Sprite.from(Resources.ExitImage);
@@ -30,11 +35,38 @@ export class LevelExit extends GameActor {
     this.offScreen = new OffScreenIndicator(this, undefined, vec(0.2, 0.2));
   }
 
+  override onInitialize(engine: Engine): void {
+    super.onInitialize(engine);
+
+    this.outlineMat = createOutlineMaterial(engine);
+    this.outlineMat.update((s: Shader) => {
+      // set color in HSL
+      s.trySetUniform("uniform3f", "u_outline_color", 285 / 360, 0.62, 0.86);
+    });
+  }
+
   override onAdd(engine: Engine): void {
     this.scene?.add(this.offScreen);
   }
 
   override onRemove(engine: Engine): void {
     this.scene?.remove(this.offScreen);
+  }
+
+  override onPreUpdate(engine: Engine, elapsed: number): void {
+    super.onPreUpdate(engine, elapsed);
+
+    if (!(this.scene instanceof GameLevel)) {
+      return;
+    }
+
+    const player = this.scene.player;
+    if (!player || !player.isInitialized) {
+      return;
+    }
+
+    if (player.giftsCollected >= player.giftsNeeded) {
+      this.graphics.material = this.outlineMat!;
+    }
   }
 }
