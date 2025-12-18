9bcc535da230152496e0a342ea1e29a5ca65aacd
Parnic <github@parnic.com>
2025-12-17 21:22:03 -0600

    design the level a bit, 5 regions total
    every attempt at adding deco has made it look like things that could be picked up or shot, so i'm just going to not do it.

--- public/levels/holiday-pack.tsx
+++ public/levels/holiday-pack.tsx
@@ -61,4 +61,36 @@
    <frame tileid="52" duration="290"/>
   </animation>
  </tile>
+ <wangsets>
+  <wangset name="winter" type="mixed" tile="-1">
+   <wangcolor name="grass" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="ice" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="1,1,1,2,1,1,1,1"/>
+   <wangtile tileid="1" wangid="1,1,1,2,2,2,1,1"/>
+   <wangtile tileid="2" wangid="1,1,1,1,1,2,1,1"/>
+   <wangtile tileid="3" wangid="2,2,1,1,1,2,2,2"/>
+   <wangtile tileid="4" wangid="2,2,2,2,1,1,1,2"/>
+   <wangtile tileid="5" wangid="2,2,2,1,2,2,2,2"/>
+   <wangtile tileid="6" wangid="2,2,2,1,1,1,2,2"/>
+   <wangtile tileid="7" wangid="2,2,2,2,2,1,2,2"/>
+   <wangtile tileid="8" wangid="1,1,2,2,2,1,1,1"/>
+   <wangtile tileid="9" wangid="1,1,1,1,2,2,2,1"/>
+   <wangtile tileid="10" wangid="1,2,2,2,1,1,1,1"/>
+   <wangtile tileid="11" wangid="2,2,2,2,2,2,2,2"/>
+   <wangtile tileid="12" wangid="1,1,1,1,1,2,2,2"/>
+   <wangtile tileid="13" wangid="1,1,1,2,2,2,2,2"/>
+   <wangtile tileid="14" wangid="1,2,2,2,2,2,1,1"/>
+   <wangtile tileid="15" wangid="2,1,1,1,2,2,2,2"/>
+   <wangtile tileid="16" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="17" wangid="2,2,2,2,2,1,1,1"/>
+   <wangtile tileid="18" wangid="2,2,2,1,1,1,1,1"/>
+   <wangtile tileid="19" wangid="2,1,1,1,1,1,2,2"/>
+   <wangtile tileid="20" wangid="1,2,1,1,1,1,1,1"/>
+   <wangtile tileid="21" wangid="2,2,1,1,1,1,1,2"/>
+   <wangtile tileid="22" wangid="1,1,1,1,1,1,1,2"/>
+   <wangtile tileid="25" wangid="2,1,2,2,2,2,2,2"/>
+   <wangtile tileid="26" wangid="1,1,2,2,2,2,2,1"/>
+   <wangtile tileid="27" wangid="2,2,2,2,2,2,2,1"/>
+  </wangset>
+ </wangsets>
 </tileset>
